8feae93e68d25c8b38901f23fbb930862af3f49d
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-27 17:02:13 -0500

    Edited layout for mobile phones. Added function to start button to show quiz page.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 console.log("Javascript loaded");
 
+//pages/divs
 const startPage = document.querySelector(".start");
 const quiz = document.querySelector(".quiz");
 const final = document.querySelector(".final");
 
-startPage.style.display = "none";
-quiz.style.display = "visible";
+//buttons
+const startButton = document.querySelector("#start");
+const nextButton = document.querySelector("#next");
+const replayButton = document.querySelector("#replay");
+
+//default display upon load
+startPage.style.display = "visible";
+quiz.style.display = "none";
 final.style.display = "none";
+
+
+
+
+/******PLAYING THE GAME******/
+
+//click button to start trivia
+startButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    startPage.style.display = "none";
+    quiz.style.display = "block";
+});
