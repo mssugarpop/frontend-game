1ca158843ad407dfa5da7e12939fb198cbdc7277
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-26 20:45:46 -0500

    Edited divs dimensions.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 console.log("Javascript loaded");
 
-const startPage = document.querySelector(".startTrivia");
+const startPage = document.querySelector(".start");
 const quiz = document.querySelector(".quiz");
+const final = document.querySelector(".final");
+
+startPage.style.display = "none";
+quiz.style.display = "visible";
+final.style.display = "none";
