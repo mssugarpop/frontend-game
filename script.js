4ae40c77cf2f352e555ce85175044f67ad571cdf
Erika Sanchez <erika.walker.business@gmail.com>
2021-02-27 23:30:44 -0500

    Changing ways of storing and collecting information with class implementation

--- script.js
+++ script.js
@@ -12,51 +12,6 @@ const replayButton = document.querySelector("#replay");
 
 //QUESTION
 let question = document.querySelector("#question");
-
-//SCORE
-let score = document.querySelector("#score");
-
-//OPTIONS
-let option1 = document.querySelector("#a1");
-let option2 = document.querySelector("#a2");
-let option3 = document.querySelector("#a3");
-let option4 = document.querySelector("#a4");
-
-
-//default display upon load
-startPage.style.display = "visible";
-quiz.style.display = "none";
-final.style.display = "none";
-
-
-
-
-/******PLAYING THE GAME******/
-
-/***BUTTONS***/
-//click button to start trivia
-startButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    startPage.style.display = "none";
-    quiz.style.display = "block";
-});
-
-//click next button to go to next question
-//for now using it to test going to the final page
-nextButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    quiz.style.display = "none";
-    final.style.display = "block";
-});
-
-//click 'try again' to start the trivia over
-replayButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    final.style.display = "none";
-    startPage.style.display = "block";
-});
-
-
 /***QUESTIONS***/
 //Making arrays so that way I can populate the spaces with the required questions and answers. 
 
@@ -86,6 +41,74 @@ qNineOptions = ["Sylph & Nu Mou", "Bangaa & Aegyl", "Viera & Hrothgar", "Fal'Cie
 qTenOptions = ["Pohtahto", "Popoto", "Puputu", "Lalafell"];
 
 
+//SCORE
+let score = document.querySelector("#score");
+let currentScore = 0;
+
+//OPTIONS
+let option1 = document.querySelector("#a1");
+let option2 = document.querySelector("#a2");
+let option3 = document.querySelector("#a3");
+let option4 = document.querySelector("#a4");
+
+
+//default display upon load
+startPage.style.display = "visible";
+quiz.style.display = "none";
+final.style.display = "none";
+
+
+class questionSet {
+    constructor (question, a1, a2, a3, a4, answer) {
+        this.question = question;
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+        this.answer = answer;
+    }
+}
+
+let question2 = new questionSet (questions[0], qTwoOptions[0], qTwoOptions[1], qTwoOptions[2], qTwoOptions[3], qTwoOptions[2])
+console.log(question2)
+
+
+
+
+
+/******PLAYING THE GAME******/
+
+/***BUTTONS***/
+//click button to start trivia
+startButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    startPage.style.display = "none";
+    quiz.style.display = "block";
+});
+
+//click next button to go to next question
+//for now using it to test going to the final page
+nextButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    quiz.style.display = "none";
+    // question3.style.display = "block;"
+    final.style.display = "block";
+    return currentScore;
+});
+
+//click 'try again' to start the trivia over
+replayButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    final.style.display = "none";
+    startPage.style.display = "block";
+    currentScore = 0;
+    return currentScore;
+});
+
+
+
+
+
 //Maybe doing a loop where each click on Next brings it to the next in the loop?
 //Going to need functions for each option button
 
@@ -101,3 +124,58 @@ option1.innerHTML = qOneOptions[0];
 option2.innerHTML = qOneOptions[1];
 option3.innerHTML = qOneOptions[2];
 option4.innerHTML = qOneOptions[3];
+let playerChoice = "";
+let correctAnswer = qOneOptions[3];
+
+
+option1.addEventListener('click', (e) => {
+    e.preventDefault();
+    let playerChoice = qOneOptions[0];
+    if (playerChoice === correctAnswer) {
+        console.log("That is correct");
+        currentScore++;
+        console.log(currentScore);
+    } else {
+        console.log("Incorrect answer");
+    }
+})
+
+option2.addEventListener('click', (e) => {
+    e.preventDefault();
+    let playerChoice = qOneOptions[1];
+    if (playerChoice === correctAnswer) {
+        console.log("That is correct");
+        currentScore++;
+        console.log(currentScore);
+    } else {
+        console.log("Incorrect answer");
+    }
+})
+
+option3.addEventListener('click', (e) => {
+    e.preventDefault();
+    let playerChoice = qOneOptions[2];
+    if (playerChoice === correctAnswer) {
+        console.log("That is correct");
+        currentScore++;
+        console.log(currentScore);
+    } else {
+        console.log("Incorrect answer");
+    }
+})
+
+option4.addEventListener('click', (e) => {
+    e.preventDefault();
+    let playerChoice = qOneOptions[3];
+    if (playerChoice === correctAnswer) {
+        console.log("That is correct");
+        currentScore++;
+        console.log(currentScore);
+    } else {
+        console.log("Incorrect answer");
+    }
+})
+
+
+
+
